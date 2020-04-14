2a5571d6b46ac9dfe3880e2673d6037bee12a813
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-14 17:24:43 +0200

    Update main.cpp

--- QtProject/Lab2/main.cpp
+++ QtProject/Lab2/main.cpp
@@ -38,74 +38,15 @@ for (countt=0; countt<length; countt++)
 
 
 //Vigenere cipher encryption
-int main()
-{
-    string password;
-    string key ;
-    string result;
-
-    int k=0;
-
-    std::cout<<"Enter the password to be encrypted"<<std::endl;
-    std::cin>>password;
-    std::cout<<"Enter the keyword"<<std::endl;
-    std::cin>>key;
-    for (int i=0;i<password.length();i++)
-    {
-        result[i]=(((password[i]-97)+(key[k]-97))%26)+97;
-        k++;
-        if(k==6)
-            k=0;
-    }
-    std::cout<<"     \n\n\n";
-    for(int i=0; i<password.length();i++);
-
-   return 0;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-//Caesar cipher decryption
 
 int main()
 {
-std::string password;
-int countt =0;
-int length;
-std::cout<<"Input your password"<<std::endl;
-std::getline(std::cin,password);
-length= (int)password.length();
-for (countt=0; countt<length; countt++)
-{
-    if (isalpha(password[countt]))
-
-    {
-        password[countt] = tolower(password[countt]);
-        for (int i = 0; i<13;i++)
-        {
-            if(password[countt]=='a')
-               password[countt]='z';
-            else
-                password[countt]--;
-
-        }
-
-    }
-}
-    std::cout<<"Your encrypted password is:  " <<password<<std::endl;
-
-    return 0;
-
-}
+  Vigenere cipher("VIGENERECIPHER");
+ 
+  string original = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!";
+  string encrypted = cipher.encrypt(original);
+ 
+ 
+  cout << original << endl;
+  cout << "Encrypted"<< encrypted<<endl;
 
