f1d337d60020e628c4ce3ffc79f93c4906cda414
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-14 17:30:16 +0200

    Update main.cpp

--- QtProject/Lab2/main.cpp
+++ QtProject/Lab2/main.cpp
@@ -36,6 +36,42 @@ for (countt=0; countt<length; countt++)
 
 }
 
+//caeser cipher decryption 
+
+
+int main()
+{
+std::string password;
+int countt =0;
+int length;
+std::cout<<"Input your Encrypted password"<<std::endl;
+std::getline(std::cin,password);
+length= (int)password.length();
+for (countt=0; countt<length; countt++)
+{
+    if (isalpha(password[countt]))
+
+    {
+        password[countt] = tolower(password[countt]);
+        for (int i = 0; i<13;i++)
+        {
+            if(password[countt]=='a')
+               password[countt]='z';
+            else
+                password[countt]--;
+
+        }
+
+    }
+}
+    std::cout<<"Your decrypted password is:  " <<password<<std::endl;
+
+    return 0;
+
+}
+
+
+
 
 //Vigenere cipher encryption
 
@@ -49,4 +85,20 @@ int main()
  
   cout << original << endl;
   cout << "Encrypted"<< encrypted<<endl;
+
+
+//vigenere cypher decryption
+
+int main()
+{
+  Vigenere cipher("VIGENERECIPHER");
+ 
+  string original = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!";
+  
+  string decrypted = cipher.decrypt(encrypted);
+ 
+  cout << original << endl;
+  
+  cout << "Decrypted: " << decrypted << endl;
+}
 
