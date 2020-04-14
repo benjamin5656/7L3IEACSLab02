4b610748901b6414c2295a4a994d741fd747b4e7
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-14 17:36:46 +0200

    Update problem2.cc

--- QtProject/Lab2/problem2.cc
+++ QtProject/Lab2/problem2.cc
@@ -40,11 +40,50 @@ for (countt=0; countt<length; countt++)
 
 }
 
+// decryptinh vigenere cipher
+
+
+
+class Vigenere
+{
+public:
+  string key;
+ 
+  Vigenere(string key)
+  {
+    for(int i = 0; i < key.size(); ++i)
+    {
+      if(key[i] >= 'A' && key[i] <= 'Z')
+        this->key += key[i];
+      else if(key[i] >= 'a' && key[i] <= 'z')
+        this->key += key[i] + 'A' - 'a';
+    }
+  }
+ 
 
 
 
 
-
+string decrypt(string text)
+  {
+    string out;
+ 
+    for(int i = 0, j = 0; i < text.length(); ++i)
+    {
+      char c = text[i];
+ 
+      if(c >= 'a' && c <= 'z')
+        c += 'A' - 'a';
+      else if(c < 'A' || c > 'Z')
+        continue;
+ 
+      out += (c - key[j] + 26) % 26 + 'A'; 
+      j = (j + 1) % key.length();
+    }
+ 
+    return out;
+  }
+};
 
 
 
