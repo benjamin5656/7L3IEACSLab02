1ef0ee7db2767350f0d98cbdb9e380027bd3c8c9
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-14 14:39:44 +0800

    Update main.cpp

--- QtProject/Lab2/main.cpp
+++ QtProject/Lab2/main.cpp
@@ -40,3 +40,42 @@ for (countt=0; countt<length; countt++)
 
 
 
+
+
+
+
+
+
+//Caesar cipher decryption
+
+int main()
+{
+std::string password;
+int countt =0;
+int length;
+std::cout<<"Input your password"<<std::endl;
+std::getline(std::cin,password);
+length= (int)password.length();
+for (countt=0; countt<length; countt++)
+{
+    if (isalpha(password[countt]))
+
+    {
+        password[countt] = tolower(password[countt]);
+        for (int i = 0; i<13;i++)
+        {
+            if(password[countt]=='a')
+               password[countt]='z';
+            else
+                password[countt]--;
+
+        }
+
+    }
+}
+    std::cout<<"Your encrypted password is:  " <<password<<std::endl;
+
+    return 0;
+
+}
+
