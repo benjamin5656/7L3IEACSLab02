ebf536b1057e1585ff0488e63fe30c6b58ff1911
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-14 14:14:29 +0800

    Update problem1.cc

--- QtProject/Lab2/problem1.cc
+++ QtProject/Lab2/problem1.cc
@@ -1,5 +1,39 @@
 #include "problem1.h"
 
-namespace problem1 {
+#include <string>;
+#include <iostream>;
+
+using namespace std;
+
+string show(string input)
+{
+
+ input;
+std::cout<<"Enter an input"<<std::endl;
+std::cin>>input;
+int countt =0;
+int length;
+
+std::getline(std::cin,input);
+length= (int)input.length();
+for (countt=0; countt<length; countt++)
+{
+    if (isalpha(input[countt]))
+
+    {
+        input[countt] = tolower(input[countt]);
+        for (int i = 0; i<13;i++)
+        {
+            if(input[countt]=='z')
+               input[countt]='a';
+            else
+                input[countt]++;
+
+        }
+
+    }
+}
+   return input;
+
 
 }
