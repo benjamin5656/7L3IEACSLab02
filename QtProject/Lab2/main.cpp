788e6566d4cf3ec1296b55530de008d7f7bbea55
benjamin5656 <56698040+benjamin5656@users.noreply.github.com>
2020-04-17 13:46:48 +0800

    Update main.cpp

--- QtProject/Lab2/main.cpp
+++ QtProject/Lab2/main.cpp
@@ -72,34 +72,160 @@ for (countt=0; countt<length; countt++)
 }
 
 
-
-
 //Vigenere cipher encryption
 
-int main()
+ class Vigenere
+
 {
-  Vigenere cipher("VIGENERECIPHER");
- 
-  string original = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!";
-  string encrypted = cipher.encrypt(original);
- 
- 
-  cout << original << endl;
-  cout << "Encrypted"<< encrypted<<endl;
 
+    public:
 
-//vigenere cypher decryption
+        string key;
 
-int main()
+Vigenere(string key)
+
+        {
+
+for (int i = 0; i <key.size(); ++i)
+
+            {
+
+                if (key[i] >= 'A' && key[i] <= 'Z')
+
+                    this->key += key[i];
+
+                else if (key[i] >= 'a' && key[i] <= 'z')
+
+                    this->key += key[i] + 'A' - 'a';
+
+            }
+
+        }                                             
+
+string encrypt(string text)
+
+        {
+
+            string out;
+
+for (int i = 0, j = 0; i <text.length(); ++i)
+
+            {
+
+                char c = text[i];
+
+             if (c >= 'a' && c <= 'z')
+
+                    c += 'A' - 'a';
+
+                else if (c < 'A' || c > 'Z')
+
+                    continue;
+
+                out += (c + key[j] - 2 * 'A') % 26 + 'A';
+
+                j = (j + 1) % key.length();
+
+            }
+
+            return out;
+
+       }
+
+      int main()
+
 {
-  Vigenere cipher("VIGENERECIPHER");
- 
-  string original = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!";
-  
+
+Vigenere cipher("VIGENERECIPHER");
+
+string password ;
+ std::cout<<"Enter your password to be encrypted"<<std::endl;
+  std::cin>>password;
+ string encrypted = cipher.encrypt(password);
+std::cout<<"password"<<endl;
+std::cout<<"Encrypted: "<<encrypted<<endl;
+          
+   
+          
+  //vigernere cipher decryption      
+          
+  class Vigenere
+
+{
+
+    public:
+
+        string key;
+
+Vigenere(string key)
+
+        {
+
+for (int i = 0; i <key.size(); ++i)
+
+            {
+
+                if (key[i] >= 'A' && key[i] <= 'Z')
+
+                    this->key += key[i];
+
+                else if (key[i] >= 'a' && key[i] <= 'z')
+
+                    this->key += key[i] + 'A' - 'a';
+
+            }
+
+        }                                             
+
+      
+       string decrypt(string text)
+
+        {
+
+            string out;
+
+for (int i = 0, j = 0; i <text.length(); ++i)
+
+            {
+
+                char c = text[i];
+
+                if (c >= 'a' && c <= 'z')
+
+                    c += 'A' - 'a';
+
+                else if (c < 'A' || c > 'Z')
+
+                    continue;
+
+                out += (c - key[j] + 26) % 26 + 'A';
+
+                j = (j + 1) % key.length();
+
+            }
+
+            return out;
+
+ }
+
+};
+          
+    int main()
+
+{
+
+Vigenere cipher("VIGENERECIPHER");
+
+    string password ;
+   std::cout<<"Enter your encrypted password"<<std::endl;
+   std:cin>>password;     
+        
   string decrypted = cipher.decrypt(encrypted);
- 
-  cout << original << endl;
-  
-  cout << "Decrypted: " << decrypted << endl;
+
+cout<<"password"<<endl;
+
+cout<<"Decrypted: "<<decrypted<<endl;
+
 }
 
+       
